3852bd1460cf7b81311e0eabbe7158f50ec264d7
Alexander Guryanov <caiiiycuk@gmail.com>
2021-04-14 16:41:25 +0700

    new emulators

--- public/emulators-ui/types/js-dos.d.ts
+++ public/emulators-ui/types/js-dos.d.ts
@@ -1,16 +1,19 @@
 import { CommandInterface } from "emulators";
+import { TransportLayer } from "emulators/dist/types/protocol/protocol";
 import { EmulatorsUi } from "./emulators-ui";
 import { Layers, LayersOptions } from "./dom/layers";
-export declare type EmulatorFunction = "dosWorker" | "dosDirect" | "janus";
+export declare type EmulatorFunction = "dosboxWorker" | "dosboxDirect" | "dosboxNode" | "janus" | "backend";
 export interface DosOptions {
     emulatorFunction?: EmulatorFunction;
     clickToStart?: boolean;
     layersOptions?: LayersOptions;
+    createTransportLayer?: () => TransportLayer;
 }
 export declare class DosInstance {
     static initialRun: boolean;
     emulatorsUi: EmulatorsUi;
     emulatorFunction: EmulatorFunction;
+    createTransportLayer?: () => TransportLayer;
     layers: Layers;
     ciPromise?: Promise<CommandInterface>;
     enableMobileControls: () => void;
