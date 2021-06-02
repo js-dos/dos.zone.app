308cca6e698981772cb428efd6ceacbbb2d01a19
Alexander Guryanov <caiiiycuk@gmail.com>
2021-06-02 22:54:21 +0700

    handle back button correctly on android for youtube-modal

--- src/pages/components/youtube-modal.tsx
+++ src/pages/components/youtube-modal.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 
 import { Overlay, Callout } from "@blueprintjs/core";
 import { Youtube } from "./youtube";
+import { BackButton } from "../../cap-config";
 
 export function YoutubeModal(props: { url?: string, onClose: () => void }) {
+    useEffect(() => {
+        if (props.url === undefined) {
+            return;
+        }
+
+        const prevHandler = BackButton.customHandler;
+        BackButton.customHandler = () => props.onClose();
+
+        return () => {
+            BackButton.customHandler = prevHandler;
+        };
+    }, [props.url, props.onClose]);
+
     if (props.url === undefined) {
         return null;
     }
